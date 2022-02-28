9fad650b2f88812573cc2e7fd4fd287d6a50c9aa
azazel <azaz3l0z@protonmail.com>
2022-02-28 01:04:32 +0100

    First commit

---
+++ src/main/java/modules/ScrapPython.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modules;
+
+import org.python.util.PythonInterpreter; 
+
+/**
+ *
+ * @author azazel
+ */
+public class ScrapPython {
+    public ScrapPython(String site, String trademark, String model, String yearstart, String yearend, String change, String km){
+    	System.out.println(trademark);
+    }
+}
