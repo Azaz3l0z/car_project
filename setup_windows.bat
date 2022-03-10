03d17311161f1a252965570106d500421c6b2624
Azaz3l0z <99992552+Azaz3l0z@users.noreply.github.com>
2022-03-10 03:57:14 +0100

    Add files via upload

--- setup_windows.bat
+++ setup_windows.bat
@@ -1,28 +1,35 @@
 cd resources
-mkdir python/env
-python -m venv python/env
-python/env/Scripts/python3.exe
-pip install -r python/requirements.txt
 
 :: Executable run
 cd python
+python -m venv env
+call env\Scripts\activate
+pip install -r requirements.txt
 pyinstaller --onefile --nowindow run.py
-rmdir /s build
+
+rmdir /Q /s build
 del run.spec
-move "dist/run.exe" .
-rmdir /s dist
-rmdir /s __pycache__
-rmdir /s env
+move "dist\run.exe" .
+rmdir /Q /s dist
+rmdir /Q /s __pycache__
+call deactivate
+rmdir /Q /s env
 cd ..
 
 :: Executable updater
 cd updater
+python -m venv env
+call env\Scripts\activate.bat
+pip install -r requirements.txt
 pyinstaller --onefile --nowindow updater.py
-rmdir /s build
+
+rmdir /Q /s build
 del updater.spec
-move "dist/updater.exe" .
-rmdir /s dist
-rmdir /s __pycache__
-rmdir /s env
+move "dist\updater.exe" .
+rmdir /Q /s dist
+rmdir /Q /s __pycache__
+call deactivate
+rmdir /Q /s env
 cd ..
 
+cd ..
